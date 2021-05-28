c09e20e75fc2d5dfcf98e64736b625a942b480ed
Giulio Riondino <riondino.giulio@gmail.com>
2021-05-28 08:19:39 -0700

    second try for models assignment

--- mysite/student/models.py
+++ mysite/student/models.py
@@ -6,24 +6,46 @@ from datetime import datetime
 class School(models.Model):
     name = models.CharField(max_length = 400)
 
+    def __str__(self):
+        return self.name
+
 class Grade(models.Model):
-    type = models.IntegerField()
+    name = models.CharField(max_length = 1)
+
+    def __str__(self):
+        return self.name
+
 
 class Certificate_type(models.Model):
     name = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return self.name
+
+
 class Faculty(models.Model):
     name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.name
+
 
 class Department(models.Model):
     name = models.CharField(max_length = 50)
     faculty = models.ForeignKey(Faculty, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+
 
 class Student(models.Model):
         school = models.ForeignKey(School, on_delete=models.PROTECT, default = 0)
         first_name = models.CharField(max_length = 50)
         last_name = models.CharField(max_length = 50)
         age = models.IntegerField()
-        department = models.CharField(max_length = 50)
-        grade = models.ForeignKey(Grade, on_delete=models.PROTECT)
-        certificate_type = models.CharField(max_length = 50)  
+        department = models.ForeignKey(Department, on_delete=models.PROTECT)
+        grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
+        certificate_type = models.ForeignKey(Certificate_type, on_delete=models.PROTECT)
+
+        def __str__(self):
+            return self.first_name
